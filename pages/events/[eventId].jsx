46cfea1eb3fe38d89282e9ef2d159412771bc0cd
Ashutosh Tiwari <ashutosh@Apples-MacBook-Pro.local>
2024-04-11 15:04:27 +0530

    add event details page

--- pages/events/[eventId].jsx
+++ pages/events/[eventId].jsx
@@ -1,5 +1,31 @@
 import React from "react";
-
+import { useRouter } from "next/router";
+import { getEventById } from "@/dummtData";
+import EventSummary from "@/components/event-detail/event-summary";
+import EventLogistics from "@/components/event-detail/event-logistics";
+import EventContent from "@/components/event-detail/event-content";
 export default function EventDetails() {
-  return <div>EventDetails</div>;
+  const router = useRouter();
+  const { eventId } = router.query;
+  const event = getEventById(eventId);
+  if (!event)
+    return (
+      <div>
+        <h1>Not found...</h1>
+      </div>
+    );
+  return (
+    <>
+      <EventSummary title={event.title} />
+      <EventLogistics
+        data={event.date}
+        imageAlt={event.title}
+        address={event.address}
+        image={event.image}
+      />
+      <EventContent>
+        <p>{event.description}</p>
+      </EventContent>
+    </>
+  );
 }
