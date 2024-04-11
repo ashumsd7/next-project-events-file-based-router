4bcb51ee7ff798548af1549efa475f4f11fe0c41
Ashutosh Tiwari <ashutosh@Apples-MacBook-Pro.local>
2024-04-11 14:43:50 +0530

    add styles

--- components/events/event-item.jsx
+++ components/events/event-item.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Link from "next/link";
+import classes from "./event-item.module.css";
 function EventItem({ item }) {
   const humanReadabelDate = new Date(item.date).toLocaleDateString("en-us", {
     date: "numeric",
@@ -10,19 +11,19 @@ function EventItem({ item }) {
   const formattedAddress = item.location.replace(", ", "\n");
   const exploreLink = `/events/${item.id}`;
   return (
-    <li>
+    <li className={classes.item}>
       <img src={"/" + item.image} alt=""></img>
-      <div>
-        <div>
+      <div className={classes.content}>
+        <div className={classes.summary}>
           <h2>{item.title}</h2>
           <div>
             <time>{humanReadabelDate}</time>
           </div>
           <div>
-            <address>{formattedAddress}</address>
+            <address className={classes.address}>{formattedAddress}</address>
           </div>
         </div>
-        <div>
+        <div className={classes.actions}>
           <Link href={exploreLink}></Link>
         </div>
       </div>
--- components/events/event-list.jsx
+++ components/events/event-list.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import EventItem from "./event-item";
-
+import classes from "./event-list.module.css";
 function EventList(props) {
   const { items } = props;
   return (
-    <ul>
+    <ul className={classes.list}>
       {items?.map((item) => {
         return <EventItem item={item} />;
       })}
