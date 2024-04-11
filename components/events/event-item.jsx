22bdca668e5eb1d99d384c8dd2b3b188b9ce6e96
Ashutosh Tiwari <ashutosh@Apples-MacBook-Pro.local>
2024-04-11 14:55:47 +0530

    use icons

--- components/events/event-item.jsx
+++ components/events/event-item.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import Link from "next/link";
 import classes from "./event-item.module.css";
 import Button from "../ui/button";
+import DateIcon from "../icons/date-icon";
+import ArrowIcon from "../icons/arrow-right-icon";
+import Addressicon from "../icons/address-icon";
 function EventItem({ item }) {
   const humanReadabelDate = new Date(item.date).toLocaleDateString("en-us", {
     date: "numeric",
@@ -17,15 +20,23 @@ function EventItem({ item }) {
       <div className={classes.content}>
         <div className={classes.summary}>
           <h2>{item.title}</h2>
-          <div>
+          <div className={classes.date}>
+            <DateIcon />
             <time>{humanReadabelDate}</time>
           </div>
-          <div>
+          <div className={classes.address}>
+            <Addressicon />{" "}
             <address className={classes.address}>{formattedAddress}</address>
           </div>
         </div>
         <div className={classes.actions}>
-          <Button link={exploreLink}>Explore Event</Button>
+          <Button link={exploreLink}>
+            {" "}
+            <span className={classes.icon}>
+              <ArrowIcon />
+            </span>
+            <span> Explore Event</span>
+          </Button>
         </div>
       </div>
     </li>
