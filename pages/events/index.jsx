90b65506ab3f30f1fcd1a4b08ae3caff7d5dce3f
Ashutosh Tiwari <ashutosh@Apples-MacBook-Pro.local>
2024-04-11 15:16:56 +0530

    add all events page

--- pages/events/index.jsx
+++ pages/events/index.jsx
@@ -1,12 +1,11 @@
+import EventList from "@/components/events/event-list";
+import { getAllEvents } from "@/dummtData";
 import Link from "next/link";
 import React from "react";
 
 const AllEvents = () => {
-  return (
-    <div>
-      <Link href="/">Hey</Link>
-    </div>
-  );
+  const events = getAllEvents();
+  return <EventList items={events} />;
 };
 
 export default AllEvents;
